713186343786d4aa3ed251dc0575872ec6bf0ff1
sandeep kumar mahato <sandeepkumarmahato712@gmail.com>
2023-01-07 13:42:14 +0530

    init

--- script.js
+++ script.js
@@ -17,28 +17,18 @@ const getWeather = (city) => {
       console.log(response);
 
       /* cloud_pct.innerHTML = response.cloud_pct; */
-      cloud_pct3.innerHTML = response.cloud_pct;
       temp.innerHTML = response.temp;
       temp2.innerHTML = response.temp;
-      temp3.innerHTML = response.temp;
       feels_like.innerHTML = response.feels_like;
-      feels_like3.innerHTML = response.feels_like;
       humidity.innerHTML = response.humidity;
       humidity2.innerHTML = response.humidity;
-      humidity3.innerHTML = response.humidity;
       min_temp.innerHTML = response.min_temp;
-      min_temp3.innerHTML = response.min_temp;
       max_temp.innerHTML = response.max_temp;
-      max_temp3.innerHTML = response.max_temp;
       wind_speed.innerHTML = response.wind_speed;
       wind_speed2.innerHTML = response.wind_speed;
-      wind_speed3.innerHTML = response.wind_speed;
       wind_degrees.innerHTML = response.wind_degrees;
-      wind_degrees3.innerHTML = response.wind_degrees;
       sunrise.innerHTML = response.sunrise;
-      sunrise3.innerHTML = response.sunrise;
       sunset.innerHTML = response.sunset;
-      sunset3.innerHTML = response.sunset;
     })
     .catch((err) => console.error(err));
 };
@@ -49,6 +39,27 @@ submit.addEventListener("click", (e) => {
 });
 
 getWeather("Delhi");
+
+fetch(
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+
+    delhi_cloud_pct.innerHTML = response.cloud_pct;
+    delhi_temp.innerHTML = response.temp;
+    delhi_feels_like.innerHTML = response.feels_like;
+    delhi_humidity.innerHTML = response.humidity;
+    delhi_min_temp.innerHTML = response.min_temp;
+    delhi_max_temp.innerHTML = response.max_temp;
+    delhi_wind_speed.innerHTML = response.wind_speed;
+    delhi_wind_degrees.innerHTML = response.wind_degrees;
+    delhi_sunrise.innerHTML = response.sunrise;
+    delhi_sunset.innerHTML = response.sunset;
+  })
+  .catch((err) => console.error(err));
 
 fetch(
   "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Mumbai",
